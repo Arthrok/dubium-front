9da53f2f1c104a9e5602ee6bf8dcba4950782c68
Arthrok <98776585+Arthrok@users.noreply.github.com>
2023-06-06 19:11:46 -0300

    .

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "https://dubium-back.vercel.app",
+  baseURL: "https://dubium.herokuapp.com",
 });
 
 // apiRequest.defaults.headers.common['Authorization'] = "Bearer " + localStorage.getItem("token");
--- src/services/socket.js
+++ src/services/socket.js
@@ -1,4 +1,4 @@
 import io from 'socket.io-client';
 
 
-export default io('https://dubium-back.vercel.app')
+export default io('https://dubium.herokuapp.com')
